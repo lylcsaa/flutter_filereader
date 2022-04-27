792718cfde789f7214b901354f3ba5dab3eaa736
liyalou <1192033244@qq.com>
2022-04-27 10:15:26 +0800

    删除iOS依赖

--- ios/Classes/FlutterFileReaderPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterFileReaderPlugin.h"
-#import <flutter_filereader/flutter_filereader-Swift.h>
-
-@implementation FlutterFileReaderPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterFileReaderPlugin registerWithRegistrar:registrar];
-}
-@end
